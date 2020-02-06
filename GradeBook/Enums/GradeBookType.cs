181dc2a375675d39af4c05d381da801a8444e99d
Wes <wes.holton@gmail.com>
2020-02-06 15:58:42 -0500

    Add GradeBookType enum, and Standard + Ranked GradeBook classes

---
+++ GradeBook/Enums/GradeBookType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GradeBook.Enums;
+
+namespace GradeBook.Enums
+{
+    public enum GradeBookType
+    {
+        Standard,
+        Ranked,
+        ESNU,
+        OneToFour,
+        SixPoint
+    }
+}
